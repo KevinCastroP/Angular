2b667c4750910de732b1d0b82ce9291542aa1fe1
KevinCastroP <castrok.c11@hotmail.com>
2020-09-22 20:54:22 -0500

    first task

---
+++ typescript/app.js
@@ -0,0 +1,9 @@
+(function () {
+    function saludar(nombre) {
+        console.table('Hola ' + nombre); // Hola Logan
+    }
+    var wolverine = {
+        nombre: 'Logan'
+    };
+    saludar(wolverine.nombre);
+})();
---
+++ typescript/app.ts
@@ -0,0 +1,12 @@
+(function() {
+    function saludar( nombre:string ) {
+        console.table( 'Hola ' + nombre ); // Hola Logan
+    }
+    
+    
+    const wolverine = {
+        nombre: 'Logan'
+    };
+    saludar( wolverine.nombre );
+
+})();
